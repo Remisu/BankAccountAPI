84ba1fec2c03c6f5d3e94d27b6035a1586e08f22
Emerson Miguel <emersongne@gmail.com>
2023-07-18 15:19:36 +0100

    Adicionei os campos da classe account.

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,6 +1,13 @@
 ﻿namespace BankAccountAPI.Models
 {
-    public class Account
+    public class Conta
     {
+        public string NumeroConta { get; set; }
+        public string TipoConta { get; set; }
+        public decimal Saldo { get; set; }
+        public string Titular { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public List<string> HistoricoTransacoes { get; set; }
+        public string StatusConta { get; set; }
     }
 }
